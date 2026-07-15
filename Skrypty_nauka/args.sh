e98a3fe49c1b49c438ad74fb08c543975eac38e5
sewa4242 <gackawojtek@gmail.com>
2026-07-15 23:28:14 +0200

    Delete Skrypty_nauka directory

--- Skrypty_nauka/args.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Purpose: Learn how positional parametrs work  ($1, $2, $#)
-# Usage: ./args.sh  <username>  <filename> 
-# Level: Linux Essentials practice
-
-echo  "Użytkownik: $1"
-echo  "Plik: $2"
-echo   "Argumentów: $#"
